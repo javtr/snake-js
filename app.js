51e549c1458cfe1505e6444e530ea2d0861085c7
javier triana <javtriana.z@gmail.com>
2021-10-28 09:49:45 -0500

    add level html

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 import {scoreAdd,scoreReset,scoreReturn} from "./utilities.js";
 let score = document.querySelector('#score');
-
+let level = document.querySelector('#level');
 
 
 export function updateScore (){
     score.innerHTML = scoreReturn();
+    level.innerHTML = scoreReturn();
 }
 
